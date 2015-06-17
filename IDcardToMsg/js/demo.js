4c16538f6e80b7bbc35e5983dcf07c9237bcc336
qinzhen <yangjianbo516@163.com>
2015-06-17 21:35:34 +0800

    update html

--- IDcardToMsg/js/demo.js
+++ IDcardToMsg/js/demo.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
         var IDcard=$("input[name='IDcard']").val();
         var data=validIDcard(IDcard);
         if(data.pass){
-            $("span.error").html("");
+            $("div.error").html("").hide();
             var $demo=$("<tr><td class='center'>"+IDcard+"</td><td class='center'>"+data.sex+"</td><td class='center'>"+data.birthDay+"</td><td>"+data.address+"</td></tr>");
             $("table").append($demo);
         }else{
-            $("span.error").html("请输入正确的身份证号码");
+            $("div.error").html("请输入正确的身份证号码").show();
         }
     })
 })
