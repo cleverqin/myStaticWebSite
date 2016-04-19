6b29bb8068aad02a4a9327f9945ca3327dc59355
“zhenQin” <705597001@qq.com>
2016-04-19 13:34:30 +0800

    修改首页样式banner图插件

--- js/index.js
+++ js/index.js
@@ -2,7 +2,17 @@
  * Created by ghost on 2015/6/17.
  */
 $(function(){
-    $("#slidesBox").SmartSlide();
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: 5000,
+        autoplayDisableOnInteraction: false,
+        loop: true
+    });
 })
 
 
