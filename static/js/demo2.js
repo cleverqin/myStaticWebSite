81003bb33cf84a4fa36a1ae1a5b5caa6e0660ced
cleverqin <705597001@qq.com>
2017-06-09 17:23:40 +0800

    修改聊天室页面

--- static/js/demo2.js
+++ static/js/demo2.js
@@ -1,180 +1,267 @@
-window.onload=function () {
+(function () {
     String.prototype.Trim = function() {
         return this.replace(/(^\s*)|(\s*$)/g, "");
     }
-    var urlPre = "http://cors.itxti.net/?"
-    //发车、到站 查时刻
-    var url1 = "www.tuling123.com/openapi/api?";
-    var robot={
-        nickName:"机器人",
-            pic:"http://www.tuling123.com/resources/web/v4/img/personalCen/icon36.png"
+    Vue.filter('ago', function (value) {
+        // 返回处理后的值
+        return Time(value,"%h:%m")
+    })
+    var API={
+        robot:{
+            url:'http://cors.itxti.net/?www.tuling123.com/openapi/api?',
+            type:'get',
+            dataType:"json"
+        },
+        weather:{
+            url:'http://api.asilu.com/weather/',
+            type:'get',
+            dataType:"jsonp"
+        },
+        train:{
+            url:'http://cors.itxti.net/?www.webxml.com.cn/WebServices/TrainTimeWebService.asmx/getDetailInfoByTrainCode?&UserID=',
+            type:'get',
+            dataType:""
+        },
+        express:{
+            url:'http://api.asilu.com/express/',
+            type:'get',
+            dataType:"jsonp"
+        },
+        request:function (type,data,callback) {
+            var _this=this;
+            $.ajax({
+                url:_this[type].url,
+                type:_this[type].type,
+                dataType:_this[type].dataType,
+                data:data,
+                success:function (res) {
+                    callback(res)
+                },
+                error:function (error) {
+
+                }
+            })
+        }
     }
-    var face=null;
+    var robot={
+        id:"a110",
+        nickName:"机器人",
+        pic:"http://www.tuling123.com/resources/web/v4/img/personalCen/icon36.png"
+    }
+    var store={
+        state:{
+            channel:robot,
+            msg:{
+                "a110":[]
+            }
+        }
+    }
+    var obj=null;
+    var audio=document.getElementById('audio')
     var onlineItem = Vue.extend({
         template:"#onlineItem",
-        props:['user']
+        props:['user','channel','time'],
+        computed:{
+            msg:function () {
+                return store.state.msg
+            }
+        },
+        methods:{
+            changeChannel:function (user) {
+                this.$parent.channel=user;
+            },
+            getLastMsg:function () {
+                var lastMsg="";
+                var user=this.$props.user;
+                if(this.msg[user.id]){
+                    var len=this.msg[user.id].length;
+                    if(len>0){
+                        lastMsg=this.msg[user.id][len-1].content.msg;
+                    }
+                }
+                return lastMsg
+            }
+        }
     });
     var msgItem=Vue.extend({
         template:"#msgTpl",
         props:['msg']
     });
-    var login=Vue.extend({
-        template:"#loginTpl",
-        data:function () {
+    var uiAlter=Vue.extend({
+        template:"#myAlter",
+        props:['show','type'],
+        data:function(){
             return{
-                user:{
-                    nickName:"",
-                    pic:"img/headPic/1.jpg"
-                },
-                picList:['img/headPic/1.jpg',
-                    'img/headPic/2.jpg',
-                    'img/headPic/3.jpg',
-                    'img/headPic/4.jpg',
-                    'img/headPic/5.jpg',
-                    'img/headPic/6.jpg',
-                    'img/headPic/7.jpg',
-                    'img/headPic/8.jpg',
-                    'img/headPic/9.jpg',
-                    'img/headPic/10.jpg']
+                city:"",
+                com:"圆通",
+                code:'',
+                train:""
             }
-        },
-        props:['show'],
-        created:function () {
         },
         methods:{
-            hideLogin:function () {
-                this.$parent.isShow=false
+            hideAlter:function () {
+                this.$parent.show=false;
             },
-            selectUserPic:function (item) {
-                this.user.pic=item;
+            searchWeather:function () {
+                var _this=this;
+                API.request('weather',{city:_this.city},function (res) {
+                    $('.search-weather .json-renderer').jsonViewer(res);
+                })
             },
-            login:function () {
-                if(this.user.nickName.Trim()==""){
-                    document.querySelector(".error-msg-box").innerHTML="请输入正确昵称";
-                    document.querySelector(".error-msg-box").style.display="inline";
-                }else {
-                    document.querySelector(".error-msg-box").style.display="none";
-                    this.$parent.isShow=false;
-                    document.querySelector(".login-btn").style.display="none";
-                    document.getElementById("onlineUser").style.display="block";
-                    this.$parent.curUser=this.user;
-                }
+            searchExpress:function () {
+                var _this=this;
+                API.request('express',{id:_this.code,com:_this.com},function (res) {
+                    $('.search-express .json-renderer').jsonViewer(res);
+                })
+            },
+            searchTrain:function () {
+                var _this=this;
+                API.request('train',{TrainCode:_this.train},function (data) {
+                    var timeTables = $(data).find("TrainDetailInfo");
+                    var tableData=[];
+                    timeTables.each(function(index,obj){
+                        var that = $(this);
+                        if(that.find("TrainStation").text() == "数据没有被发现"){
+                            tableData.push({error:0,msg:"数据没有被发现"});
+                            return
+                        }
+                        var item={};
+                        item.TrainStation=that.find("TrainStation").text();//
+                        item.ArriveTime=that.find("ArriveTime").text();
+                        item.StartTime=that.find("StartTime").text();
+                        item.KM=that.find("KM").text();
+                        tableData.push(item)
+                    })
+                    $('.search-train .json-renderer').jsonViewer(tableData);
+                })
             }
         }
-    });
-    window.app = new Vue({
+    })
+    var app = new Vue({
         data:function () {
             return{
-                onlineNum:1,
-                isShow:false,
                 curUser:{
+                    id:'1',
                     nickName:"似水流年",
-                    pic:"../static/img/img.png"
+                    pic:"http://q.qlogo.cn/headimg_dl?dst_uin=705597001&spec=100"
                 },
-                msg:"",
-                onlineList:[{
-                    nickName:"机器人",
-                    pic:"http://www.tuling123.com/resources/web/v4/img/personalCen/icon36.png"
+                show:false,
+                showMenu:false,
+                type:"weather",
+                txt:"",
+                keywords:'',
+                time:"",
+                onlineList:[robot,{
+                    id:'b11',
+                    nickName:"温柔的荆棘",
+                    pic:"http://q.qlogo.cn/headimg_dl?dst_uin=564130874&spec=100"
                 }],
-                msgList:[
-                    {
-                        type:"sys",
-                        content:{
-                            msg:"系统消息：机器人加入了聊天室"
-                        }
-                    }
-                ]
+                msgList:[],
+                channel:{}
             }
         },
-        el: '#content',
+        computed: {},
+        watch:{
+            channel:function (curvaule,oldVaule) {
+                if(curvaule!=oldVaule){
+                    store.state.channel=this.channel;
+                    if(!store.state.msg[this.channel.id]){
+                        store.state.msg[this.channel.id]=[]
+                    }
+                    this.msgList=store.state.msg[this.channel.id]
+                    this.$nextTick(function () {
+                        this.scrollFooter()
+                    })
+                }
+            }
+        },
+        mounted:function () {
+            var self=this;
+            this.channel=store.state.channel;
+            this.msgList=store.state.msg[store.state.channel.id];
+            var msgItem={
+                type:"sys",
+                content:{
+                    msg:"系统消息：机器人加入了聊天室"
+                },
+                time:new Date().getTime()
+            }
+            this.msgList.push(msgItem);
+            obj=new Face({
+                el:document.querySelector('.web_wechat_face'),
+                callBack:function (face) {
+                    self.txt+="【"+face.title+"】";
+                }
+            })
+            document.addEventListener("click",function (e) {
+                self.showMenu=false
+            })
+        },
+        el: '#chatBox',
         components: {
             'online-item': onlineItem,
             'msg-item': msgItem,
-            'login': login
+            'ui-alter': uiAlter
         },
         methods:{
             sendMsg:function () {
-                if(this.msg.Trim()!=""){
+                if(this.txt.Trim()!=""){
+                    this.time=new Date().getTime();
                     var msgItem={
                         type:'send',
                         content:{
                             user:this.curUser,
-                            msg:face.replaceFace(this.msg)
-                        }
+                            msg:obj.replaceFace(this.txt)
+                        },
+                        time:this.time
                     }
                     this.msgList.push(msgItem);
-                    this.getMsg(this.msg);
-                    this.msg="";
-                    document.querySelector(".msg-input").focus();
+                    this.scrollFooter();
+                    this.postMsg(this.txt)
+                    this.txt="";
                 }else {
                     this.msg="";
                 }
             },
-            sendImg:function () {
-                document.querySelector(".imgInput").click();
-            },
-            getMsg:function (msg) {
+            postMsg:function (msg) {
                 var _this=this;
-                var url=urlPre+url1;
-                $.ajax({
-                    url:url,
-                    type:"get",
-                    dataType:"json",
-                    data:{
-                        key:'a36d98ad2dfa44a487c74fefff41080c',
-                        info:msg,
-                        userid:"123456"
-                    },
-                    success:function (data) {
-                       if(data.text){
-                           var msgItem={
-                               type:'user',
-                               content:{
-                                   user:robot,
-                                   msg:filterData(data)
-                               }
-                           }
-                           _this.msgList.push(msgItem);
-                       }
-                    },
-                    error:function (error) {
-
+                API.request('robot',{
+                    key:'a36d98ad2dfa44a487c74fefff41080c',
+                    info:msg,
+                    userid:"123456"
+                },function (data) {
+                    if(data.text){
+                        _this.time=new Date().getTime();
+                        var msgItem={
+                            type:'user',
+                            content:{
+                                user:_this.channel,
+                                msg:filterData(data)
+                            },
+                            time:_this.time
+                        }
+                        _this.msgList.push(msgItem);
+                        _this.$nextTick(function () {
+                            _this.scrollFooter()
+                        })
+                        audio.play();
                     }
                 })
             },
-            imgSelect:function () {
-                var _this=this;
-                var img=document.querySelector(".imgInput");
-                if (img.files.length != 0) {
-                    var file = img.files[0],
-                        reader = new FileReader();
-                    if (!reader) {
-                        img.value = '';
-                        return;
-                    };
-                    reader.onload = function(e) {
-                        img.value = '';
-                        var msgItem={
-                            type:'send',
-                            content:{
-                                user:_this.curUser,
-                                msg:'<img src="'+e.target.result+'">'
-                            }
-                        }
-                        _this.msgList.push(msgItem);
-                    };
-                    reader.readAsDataURL(file);
-                };
+            filterName:function () {
+                var arr=[];
+                var self=this
+                this.onlineList.forEach(function (item ){
+                    if(item.nickName.indexOf(self.keywords)!=-1){
+                        arr.push(item)
+                    }
+                })
+                return arr;
+            },
+            scrollFooter:function () {
+                const ul = this.$refs.list;
+                ul.scrollTop = ul.scrollHeight;
             }
-        },
-        updated:function () {
-            document.querySelector(".msg-content-box").scrollTop = document.querySelector(".msg-content-box").scrollHeight;
-        }
-    });
-     face=new Face({
-        el:document.querySelector(".faceBtn"),
-        callBack:function (face,faceWarp) {
-            app.msg+="【"+face.title+"】"
         }
     });
     function filterData(data) {
@@ -184,7 +271,7 @@ window.onload=function () {
                 return data.text
                 break;
             case 200000://链接类
-                return data.text+"<a href='"+data.url+"' class='res-link' target='_blank'>打开页页面</a>"
+                return data.text+"<a href='"+data.url+"' class='res-link' target='page'>打开页页面</a>"
                 break;
             case 302000://新闻类
                 var html=data.text+"<ul class='res-list'>";
@@ -194,7 +281,7 @@ window.onload=function () {
                 }
                 for(var i=0;i<len;i++){
                     var item=data.list[i];
-                    html+="<li><a href='"+item.detailurl+"' target='_blank'>"+item.article+"</a></li>"
+                    html+="<li><a href='"+item.detailurl+"' target='page'>"+item.article+"</a></li>"
                 }
                 html+='</li>';
                 return html;
@@ -207,7 +294,7 @@ window.onload=function () {
                 }
                 for(var i=0;i<len;i++){
                     var item=data.list[i];
-                    html+="<li><a href='"+item.detailurl+"' target='_blank'>"+item.name+"</a></li>"
+                    html+="<li><a href='"+item.detailurl+"' target='page'>"+item.name+"</a></li>"
                 }
                 html+='</li>';
                 return html;
@@ -216,4 +303,4 @@ window.onload=function () {
                 return data.text
         }
     }
-}
+})()
