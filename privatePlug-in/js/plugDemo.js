121559175a5d44875da187beb879bbe3e505ea80
cleverqin <705597001@qq.com>
2017-06-09 15:07:19 +0800

    修改个人介绍页

--- privatePlug-in/js/plugDemo.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#slidesBox").SmartSlide();
-    $('.myscroll').myScroll({
-        speed: 40, //数值越大，速度越慢
-        rowHeight: 41 //li的高度
-    });
-    $(".tapBox").selectTap();
-})
